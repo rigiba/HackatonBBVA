53b01b20cac7b5e9e2830fb84a9be769734ba3b1
Sebastian-Herrera <herrera6911sebastian@hotmail.com>
2020-10-18 02:12:59 -0500

    arregle botones, next back

--- frontend/src/components/QuestionCategory.jsx
+++ frontend/src/components/QuestionCategory.jsx
@@ -4,18 +4,35 @@ import AwesomeSlider from 'react-awesome-slider';
 
 export default class QuestionCategory extends React.Component {
   state = {
-    background_color: null,
+    // background_color: null,
+    max: this.props.questions.length,
+    selected: 0,
+  };
+
+  handleChangeQuestion = async (e) => {
+    console.log(e);
+    if (e >= this.state.max)
+      return await this.setState({ selected: this.state.max - 1 });
+    else if (e <= 0) return await this.setState({ selected: 0 });
+    await this.setState({ selected: e });
   };
 
   render() {
     return (
       <div>
         <h2>{this.props.category}</h2>
-        <AwesomeSlider>
+        <AwesomeSlider
+          buttons={false}
+          bullets={false}
+          selected={this.state.selected}>
           {this.props.questions.map((question, i) => {
             return (
               <div key={i}>
-                <QuestionsList listquestions={this.props.questions[i]} />
+                <QuestionsList
+                  selected={i}
+                  handleChangeQuestion={this.handleChangeQuestion}
+                  question={this.props.questions[i]}
+                />
               </div>
             );
           })}
--- frontend/src/components/QuestionsList.jsx
+++ frontend/src/components/QuestionsList.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import Button from '../components/common/Button';
 
 export default class QuestionsList extends React.Component {
-  // handleClick = (e) => {
-  //   e.preventDefault();
-  //   const { handleChangeQuestion, selected } = this.props;
-  //   e.target.value === 'next'
-  //     ? handleChangeQuestion(selected + 1)
-  //     : handleChangeQuestion(selected - 1);
-  // };
+  handleClick = (e) => {
+    e.preventDefault();
+    const { handleChangeQuestion, selected } = this.props;
+    e.target.value === 'next'
+      ? handleChangeQuestion(selected + 1)
+      : handleChangeQuestion(selected - 1);
+  };
 
   render() {
-    const { listquestions } = this.props;
+    const { question, selected } = this.props;
     return (
-      <div key={{}}>
-        <h1>{listquestions.question}</h1>
+      <div>
+        <span>{selected + 1}</span>
+        <h2>{question.question}?</h2>
         <Button title={`back`} route='/question' onClick={this.handleClick} />
         <Button title={`next`} route='/question' onClick={this.handleClick} />
       </div>
     );
   }
 }
+
+// Primer y ultima pregunta necesitan botón para cambiar de categoría
