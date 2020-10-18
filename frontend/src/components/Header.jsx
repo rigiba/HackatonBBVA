826d2f9534c1070cef862dcb900cd8ba1e9bf810
SergioCasallas <sergiocasallas01@gmail.com>
2020-10-18 00:14:34 -0500

    Estilos del Home

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import '../styles/header.css'
 
 const Header = () => (
-  <header>
-    <h1>Header</h1>
-    <nav>
-      <ul>
-        <li>Example</li>
-        <li>Example</li>
-        <li>Example</li>
-      </ul>
-    </nav>
+  <header className='content-info'>
+    <figure className='content-info__images'>
+     <img className="content-info__imagen" src="https://www.pngkit.com/png/detail/139-1399176_icono-tierra-png-planeta-png.png" alt="Icono Tierra Png - Planeta Png@pngkit.com"/>
+    </figure>
+    <span className="about">About Us</span>
   </header>
 );
 
--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Button from '../components/common/Button';
-
+import '../styles/welcome.css'
 export default class Welcome extends React.Component {
   render() {
     return (
-      <div>
-        <p>
-          BIENVENIDA ASDASDASDS ASDASDAS A ASDASDASDASDASD ASDASD
-          ASDASDASDASDASD
+      <div className='welcome'>
+        <h1 className="welcome__title">Bienvenido al Cambio</h1>
+        <p className="welcome__subtitle">
+          Design a tiny budgeting app that works for mobile, web, and the
+          desktop.
         </p>
         <Button title='Hacer prueba' route='/questions' />
       </div>
